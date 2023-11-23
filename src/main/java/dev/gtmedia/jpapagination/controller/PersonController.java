e199e4c7ee4128c3fbf21f2ceede668f1be5f1e9
gerald.taal <41109298+japayuki@users.noreply.github.com>
2023-11-23 15:26:24 +0100

    changed pageable paramter

--- src/main/java/dev/gtmedia/jpapagination/controller/PersonController.java
+++ src/main/java/dev/gtmedia/jpapagination/controller/PersonController.java
@@ -29,7 +29,8 @@ public class PersonController {
     public Page<Person> findAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "firstName") String sortField, @RequestParam(defaultValue = "ASC") String sortDir) {
         Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :  Sort.by(sortField).descending();
         Pageable p = PageRequest.of(page, size, sort);
-        Page<Person> pg = personRepository.findAll(p);
+        Pageable p1 = PageRequest.of(page, size);
+        Page<Person> pg = personRepository.findAll(p1);
         List<Person> people = personRepository.findAll();
         List<PersonDTO> peopleDTO = people.stream()
                 .map(pd -> new PersonDTO(
